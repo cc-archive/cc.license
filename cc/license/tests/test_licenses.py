30830f6831341e300eedbe7ef5894549481abf4a
Asheesh Laroia <asheesh@asheesh.org>
2008-04-18 14:24:16 -0700

    Exercise the sampling family

--- cc/license/tests/test_licenses.py
+++ cc/license/tests/test_licenses.py
@@ -1,5 +1,20 @@
 import nose.tools
 
+def test_find_sampling_selector():
+    from zope.interface import implementedBy
+    import cc.license
+
+    sampling_selector = cc.license.get_selector('recombo')()
+    return sampling_selector
+
+def test_find_sampling_licenses():
+    selector = test_find_sampling_selector()
+    lic = selector.by_code('sampling')
+    assert not lic.libre
+    assert lic.deprecated
+    lic = selector.by_code('sampling+')
+    assert not lic.deprecated
+
 def test_find_pd():
     from zope.interface import implementedBy
     import cc.license
