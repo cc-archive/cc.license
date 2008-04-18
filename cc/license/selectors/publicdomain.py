2ddc10883aa1f3a18f959d255a2e7abc3e1368ce
Asheesh Laroia <asheesh@asheesh.org>
2008-04-18 12:25:49 -0700

    PD selector works and has tests (so does your mom)

--- cc/license/selectors/publicdomain.py
+++ cc/license/selectors/publicdomain.py
@@ -1,10 +1,32 @@
 import RDF
-from cc.license.interfaces import ILicenseSelector
+from cc.license.interfaces import ILicenseSelector, ILicense
 import cc.license
 import cc.license.rdf_helper
 
 import zope.interface
 import glob
+import os
+
+class PdLicense(object):
+    zope.interface.implements(ILicense)
+    def __init__(self, model, uri):
+        assert uri == 'http://creativecommons.org/licenses/publicdomain/'
+        self.license_class = 'publicdomain' # LAME, should pull from RDF
+        self.version = 'Your mom' # Is there a version for the PD deed?
+        self.jurisdiction = 'Your mom' # Is there a juri?  US?  (Not in the RDF)
+        self.uri = uri
+        self.current_version = 'Your mom' # Is there versioning for PD?
+        self.deprecated = False # I think
+        self.superseded = False # I think
+        self.license_code = 'publicdomain' # Based on assertion at top of init
+        self.libre = True # Sure, I think?  Not in the RDF.
+        self._names = cc.license.rdf_helper.query_to_language_value_dict(model,
+             RDF.Uri(self.uri),
+             RDF.Uri('http://purl.org/dc/elements/1.1/title'),
+             None)
+    def name(self, language = 'en'):
+        return self._names[language]
+        
 
 class Selector(object):
     zope.interface.implements(ILicenseSelector)
@@ -24,4 +46,10 @@ class Selector(object):
         if value:
             return value
         raise "your mom"
-        
+    
+    def by_code(self, code):
+        if code == 'publicdomain':
+            return self.by_uri('http://creativecommons.org/licenses/publicdomain/')
+    
+    def by_uri(self, uri):
+        return PdLicense(self.model, uri)
--- cc/license/tests/test_license_pd.py
+++ cc/license/tests/test_license_pd.py
@@ -4,7 +4,7 @@ def test_find_pd():
     from zope.interface import implementedBy
     import cc.license
 
-    pd_selector = cc.license.get_selector('publicdomain')
+    pd_selector = cc.license.get_selector('publicdomain')()
     pd = pd_selector.by_code('publicdomain')
     return pd
 
@@ -12,4 +12,8 @@ def test_pd():
     pd = test_find_pd()
     assert pd.libre
     assert pd.jurisdiction == 'Your mom'
-
+    assert not pd.deprecated
+    assert pd.jurisdiction == 'Your mom'
+    assert pd.license_code == 'publicdomain'
+    assert pd.name() == 'Public Domain' == pd.name('en')
+    assert pd.name('hr') == u'Javna domena'
