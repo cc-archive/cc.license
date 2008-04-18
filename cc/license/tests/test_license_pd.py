270d5090342e47755b8f16d5f8512daa4fa68959
Asheesh Laroia <asheesh@asheesh.org>
2008-04-18 12:20:21 -0700

    removed spurious interface definition from pd license test

--- cc/license/tests/test_license_pd.py
+++ cc/license/tests/test_license_pd.py
@@ -13,24 +13,3 @@ def test_pd():
     assert pd.libre
     assert pd.jurisdiction == 'Your mom'
 
-
-class ILicense(Interface):
-    """License metadata for a specific license."""
-
-    license_class = Attribute(u"The license class this license belongs to.")
-
-    name = Attribute(u"The human readable name for this license.")
-    version = Attribute(u"The number version for the license.")
-    jurisdiction = Attribute(u"The jurisdiction for the license.")
-    uri = Attribute(u"The fully qualified URI of the license.")
-
-    current_version = Attribute(u"The ILicense of the current version of "
-                                "this jurisdiction + license.")
-    deprecated = Attribute(u"Boolean attribute; True if this license is "
-                           "deprecated")
-    superseded = Attribute(u"Boolean attribute; True if this license has "
-                           "been replaced with a newer version.")
-    license_code = Attribute(u"The short alpha code for this license.")
-    libre = Attribute(u"Returns True if this is a 'Libre' license.")
-
-
