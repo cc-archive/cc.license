b19746badd83190b4e908144d6bc830178445dc2
Frank Tobia <frank.tobia@gmail.com>
2008-07-21 11:33:01 -0700

    Add test to make sure certain classes are always found in cc.license, no matter where they are internally.

--- cc/license/tests/test_cc_license.py
+++ cc/license/tests/test_cc_license.py
@@ -9,3 +9,10 @@ def test_locales():
         assert type(l) == unicode
     for c in ('en', 'de', 'he', 'ja', 'fr'):
         assert c in locales
+
+def test_cc_license_classes():
+    cc_dir = dir(cc.license)
+    assert 'Jurisdiction' in cc_dir
+    assert 'License' in cc_dir
+    assert 'Question' in cc_dir
+    assert 'LicenseSelector' in cc_dir
