608f4ffd7c5670f16dc7b66af2d70a830dabe0f8
Frank Tobia <frank.tobia@gmail.com>
2008-07-23 12:09:33 -0700

    Provide top-level link for cc.license.CCLicenseError

--- cc/license/__init__.py
+++ cc/license/__init__.py
@@ -4,3 +4,4 @@ from lib.classes import License, Question
 from jurisdictions.classes import Jurisdiction
 from selectors.classes import LicenseSelector
 from lib.functions import locales
+from lib.exceptions import CCLicenseError
