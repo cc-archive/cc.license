b16485d5f8fce34f4089da335f3cf5f3ea63a441
Frank Tobia <frank.tobia@gmail.com>
2008-07-31 09:48:24 -0700

    Fail with a useful error message if librdf not installed. Not sure how to write a test for this one.

--- cc/license/__init__.py
+++ cc/license/__init__.py
@@ -5,3 +5,9 @@ from jurisdictions.classes import Jurisdiction
 from selectors.classes import LicenseSelector
 from lib.functions import locales
 from lib.exceptions import CCLicenseError
+
+# fail with a useful error message if librdf not installed
+try:
+    import RDF
+except ImportError:
+    raise CCLicenseError, "Redland RDF library (librdf) not installed"
