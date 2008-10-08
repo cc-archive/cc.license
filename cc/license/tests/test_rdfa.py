9d91e0dd2d6175fe1a45d1a556a33c08133185b3
Frank Tobia <frank.tobia@gmail.com>
2008-10-08 11:12:25 -0400

    Create initial RDFa tests as their own top-level test module.

---
+++ cc/license/tests/test_rdfa.py
@@ -0,0 +1,23 @@
+"""Unit tests utilizing RDFa parsing. Primarily cc.license.formatter."""
+
+import cc.license
+import rdfadict
+import rdflib
+
+class TestHtmlFormatter:
+
+    def __init__(self):
+        self.parser = rdfadict.RdfaParser()
+        self.base = 'FOOBAR'
+        self.lic = cc.license.by_code('by')
+        self.fmtr = cc.license.formatters.HTML
+        # define namespaces
+        self.w3 = rdflib.Namespace('http://www.w3.org/1999/xhtml/vocab#')
+
+    def parse(self, rdfa_string):
+        return self.parser.parse_string(rdfa_string, self.base)
+
+    def test_basic(self):
+        r = self.fmtr.format(self.lic)
+        trips = self.parse(r)
+        assert self.lic.uri in trips[self.base][str(self.w3.license)]
