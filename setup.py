26466e6e6aab457cb28b1915641101948b14e1a0
Christopher Allan Webber <cwebber@dustycloud.org>
2009-10-29 11:36:32 -0500

    New version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 setup(name='cc.license',
-      version='0.06',
+      version='0.07',
       namespace_packages = ['cc',],
       description="License selection based on ccREL-based metadata.",
       classifiers=[],
